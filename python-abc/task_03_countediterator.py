05f5b89c57264a5c4459b4cae1b62a3cb044b689
Heidge <axel.gore35@gmail.com>
2024-10-29 17:45:47 +0100

    Fix validation for StopIteration exception

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -3,14 +3,15 @@
 class CountedIterator:
     def __init__(self, item, counter=0):
         self.item = iter(item)
+        self.item_length = len(item)
         self.counter = counter
 
     def get_count(self):
         return self.counter
 
     def __next__(self):
-        try:
+        if self.counter < self.item_length :
             self.counter += 1
             return next(self.item)
-        except:
+        else:
             raise StopIteration
