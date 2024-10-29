7014c1cee76775f07c490d37a06e1b63b13f55ff
Heidge <axel.gore35@gmail.com>
2024-10-29 17:33:11 +0100

    Add task 3 and 4

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+class CountedIterator:
+    def __init__(self, item, counter=0):
+        self.item = iter(item)
+        self.counter = counter
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        return next(self.item)
